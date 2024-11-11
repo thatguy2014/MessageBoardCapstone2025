5f13dff4ff286ece1db34fa4b25702e17264e527
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-11 13:27:33 -0500

    testing php with html
    trying to output an html page with php

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
 
-echo "Hello World!";
+?>
+
+ech <<<GFG
+            <header>
+                <div class="HeaderImg">
+                    <img src="" alt="" class="">
+                </div>
+                <h1>Ohio Northern University</h1>
+            </header>
+
+            <form action="action_page.php" method="post">
+            
+                <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required>
+            
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+            
+                <button type="submit">Login</button>
+                </div>
+            
+                <span class="psw"><a href="#">Forgot password?</a></span>
+
+            </form>
+
+            <footer>
+                <p>&copy; 2023 Your Website</p>
+            </footer>
+
+GFG;
+?>

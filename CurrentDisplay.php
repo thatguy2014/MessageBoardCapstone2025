3f1593be92ffe3d8d7ba5b2e8974bf10eb58bfc0
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-11 21:32:19 -0500

    updated error printing

--- CurrentDisplay.php
+++ CurrentDisplay.php
@@ -8,7 +8,7 @@
     $conn = mysqli_connect($host, $username, $password, $dbname);
     
     if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+        die(print("Connection failed: " . mysqli_connect_error()));
     }
 ?>
 

5a6400d52ecf7be710553b9f84b250287f325b42
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 14:37:52 -0500

    still 404ing, trying to fix
    updated .htaccess so I know it's not a problem, then made a currentdisplay.php link to the sql.php for better practice

---
+++ sql.php
@@ -0,0 +1,23 @@
+<?php
+try {
+        $conn = mysqli_init();
+        if (!$conn) {
+            throw new Exception("Failed to initialize MySQL connection");
+        }
+        
+        mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+        mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "qzmbodniyz", "YgM0Smd$bLYYepT1", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
+        
+        if (mysqli_connect_errno($conn)) {
+            throw new Exception("Failed to connect to MySQL server: " . mysqli_error($con));
+        }
+    
+    } catch (Exception $e) {
+        $error_message = "An error occurred: " . $e->getMessage();
+        die(print("<div style='color: red;'>Error: $error_message</div>"));
+    } finally {
+        if (isset($con)) {
+            mysqli_close($con);
+        }
+    }
+?>

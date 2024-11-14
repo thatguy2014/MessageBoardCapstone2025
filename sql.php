61133b8ce4089d9c86c729a13482274a777b169c
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 22:15:42 -0500

    changed the way we are printing results
    hopefully this prints right and will get me the results

--- sql.php
+++ sql.php
@@ -11,9 +11,17 @@ try {
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
         
-
         $getResults = mysqli_real_query($conn, $tsql);
-        print(mysqli_use_result($conn));
+        $getResults = mysqli_use_result($conn)
+
+        while ($row=mysqli_fetch_row($result))
+        {
+        printf ("%s (%s)\n",$row[0],$row[1]);
+        }
+        // Free result set
+        mysqli_free_result($result);
+        
+
 
 
         //if (mysqli_connect_errno($conn)) {

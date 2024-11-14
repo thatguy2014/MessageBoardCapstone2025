da7f4e18ed44647911e11d3544194cb87df1e41e
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 20:37:18 -0500

    moving debug (no 404)
    moved debug after commenting what was making it 404

--- sql.php
+++ sql.php
@@ -8,18 +8,20 @@ try {
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
         
-        print("debug \n");
+        
 
-        if (mysqli_connect_errno($conn)) {
-            throw new Exception("Failed to connect to MySQL server: " . mysqli_error($con));
-        } 
+        //if (mysqli_connect_errno($conn)) {
+        //    throw new Exception("Failed to connect to MySQL server: " . mysqli_error($con));
+        //} 
             
         
     
     } catch (Exception $e) {
         $error_message = "An error occurred: " . $e->getMessage();
         die(print("<div style='color: red;'>Error: $error_message</div>"));
-    } /*finally {
+    }
+    print("debug \n");
+    /*finally {
         if (isset($con)) {
             mysqli_close($con);
         }

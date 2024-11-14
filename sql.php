71c9cf13db7555e0ed6d3423b26c32d33cf9088a
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 22:22:34 -0500

    commenting out the print and testing the

--- sql.php
+++ sql.php
@@ -9,10 +9,10 @@ try {
 
         mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-        mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
+        mysqli_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
         
         $result = mysqli_query($conn, $tsql);
-        print($result);
+        //print($result);
         /*while ($row=mysqli_fetch_row($result))
         {
         printf ("%s (%s)\n",$row[0],$row[1]);

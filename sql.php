c6090fb0a7f38df899b3b40697ca0d57c3858cb0
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 20:17:04 -0500

    more debugging (404)
    I was getting more 404 so I added more debugging

--- sql.php
+++ sql.php
@@ -8,18 +8,20 @@ try {
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
         
+        print("finished attempting to connect \n");
+
         if (mysqli_connect_errno($conn)) {
             throw new Exception("Failed to connect to MySQL server: " . mysqli_error($con));
-        } else {
-            print("connection to MySQL server succeeded");
-        }
+        } 
+            
+        
     
     } catch (Exception $e) {
         $error_message = "An error occurred: " . $e->getMessage();
         die(print("<div style='color: red;'>Error: $error_message</div>"));
-    } finally {
+    } /*finally {
         if (isset($con)) {
             mysqli_close($con);
         }
-    }
+    }*/
 ?>

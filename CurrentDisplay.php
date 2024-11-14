870bd9a81bda8e53332dd225134e12a580534b93
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 20:52:13 -0500

    added options to query
    had the query timeout and added a print for the getresults variable again

--- CurrentDisplay.php
+++ CurrentDisplay.php
@@ -4,7 +4,12 @@
    
     include('sql.php'); 
     $tsql = "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = 1";
-    $getResults = sqlsrv_query($conn, $tsql);
+    $params = array(NULL);
+    $options = array(5,true)
+    $getResults = sqlsrv_query($conn, $tsql,$params,$options);
+    if( $getResults === false ) {
+        die( print_r( sqlsrv_errors(), true));
+   }
     //mysqli_close($con);
     //print("$getResults \n");
     echo file_get_contents("CurrentDisplay.html");

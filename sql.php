163c4076cf7f41bf8e45c82c220a2ad1512fb1ac
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 20:33:18 -0500

    print debug
    printing a debug line

--- sql.php
+++ sql.php
@@ -7,12 +7,14 @@ try {
         
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
+        
+        
 
         if (mysqli_connect_errno($conn)) {
             throw new Exception("Failed to connect to MySQL server: " . mysqli_error($con));
         } 
             
-        
+        print("debug \n");
     
     } catch (Exception $e) {
         $error_message = "An error occurred: " . $e->getMessage();

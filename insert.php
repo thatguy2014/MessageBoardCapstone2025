c4e93a92234dc4aec10b1b443717dd61331de27a
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-14 00:54:11 -0500

    fixed quote errors
    also added some html around the php

--- insert.php
+++ insert.php
@@ -1,13 +1,26 @@
-<?php
-$conn = mysqli_init();
-mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
-mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Insert Page page</title>
+    </head>
+
+    <body>
+        <center>
+            <?php
+            $conn = mysqli_init();
+            mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+            mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+            mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
 
 
 
-$displaydata = $_REQUEST['Input'];
+            $displaydata = $_REQUEST['Input'];
 
-mysqli_query($conn, 'UPDATE CurrentDisplays SET CurrentDisplay = '$displaydata' WHERE UserId = 1')
+            mysqli_query($conn, "UPDATE CurrentDisplays SET CurrentDisplay = '$displaydata' WHERE UserId = 1")
 
-?>
+            ?>
+        </center>
+    </body>
+
+</html>

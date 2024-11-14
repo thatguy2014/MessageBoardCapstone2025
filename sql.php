9339526622b2133886462b9015b2ddb79ea71c8f
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-14 00:03:20 -0500

    sure

--- sql.php
+++ sql.php
@@ -6,9 +6,9 @@
         mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
-        if (mysqli_connect_errno($conn)) {
-            die('Failed to connect to MySQL: '.mysqli_connect_error());
-            }
+        //if (mysqli_connect_errno($conn)) {
+        //    die('Failed to connect to MySQL: '.mysqli_connect_error());
+        //    }
 
         //Run the Select query
     printf("Reading data from table: \n");

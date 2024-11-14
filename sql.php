9eec14d7886786fa2036175f48d6459c9df4fb37
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 23:40:25 -0500

    trying a tutotrial?

--- sql.php
+++ sql.php
@@ -1,88 +1,27 @@
 <?php
-try {
-        /*$conn = mysqli_init();
-        if (!$conn) {
-            throw new Exception("Failed to initialize MySQL connection");
-        }
+        $conn = mysqli_init();
 
         $tsql = "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = 1";
 
         mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
-        
-        $result = mysqli_real_query($conn, $tsql);
-
-        //$storeresult = mysqli_store_result($conn);
-
-        print($result);
-        //$row = mysqli_fetch_assoc($result);
-
-        if ($row) {
-            echo "<div style='color: green;'>Current Display: " . htmlspecialchars($row['CurrentDisplay']) . "</div>";
-        } else {
-            echo "<div style='color: red;'>No results found.</div>";
-        }
-
-
-
-
-
-
-
-
-
-        //mysqli_free_result($result);          //this was breaking it earlier
-        //print($result);
-        while ($row=mysqli_fetch_row($result))
-        {
-        printf ("%s (%s)\n",$row[0],$row[1]);
-        }
-        // Free result set
-        mysqli_free_result($result);
-        
-
-
-
-        //if (mysqli_connect_errno($conn)) {
-            //throw new Exception("Failed to connect to MySQL server: " . mysqli_error($conn));
-        //} 
-            
-        
-    
-    } catch (Exception $e) {
-        $error_message = "An error occurred: " . $e->getMessage();
-        die(print("<div style='color: red;'>Error: $error_message</div>"));
-    }
-    
-    /*finally {
-        if (isset($conn)) {
-            mysqli_close($conn);
-        }*/
-
-        $mysqli = new mysqli("mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
-
-        // Check connection
-        if ($mysqli -> connect_errno) {
-          echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-          exit();
+        if (mysqli_connect_errno($conn)) {
+            die('Failed to connect to MySQL: '.mysqli_connect_error());
+            }
+        if (mysqli_query($conn, '
+        CREATE TABLE Products (
+        `Id` INT NOT NULL AUTO_INCREMENT ,
+        `ProductName` VARCHAR(200) NOT NULL ,
+        `Color` VARCHAR(50) NOT NULL ,
+        `Price` DOUBLE NOT NULL ,
+        PRIMARY KEY (`Id`)
+        );
+        ')) {
+        printf("Table created\n");
         }
         
-        $mysqli -> real_query("SELECT * FROM CurrentDisplays");
-        
-        if ($mysqli -> field_count) {
-          $result = $mysqli -> store_result();
-          $row = $result -> fetch_row();
-          printf ("%s (%s)\n", $row[0], $row[1]);
-          // Free result set
-          $result -> free_result();
-        }
-        
-        $mysqli -> close();
-
-
-
-
-
-    }
+        //Close the connection
+        mysqli_close($conn);
+        ?>
 ?>

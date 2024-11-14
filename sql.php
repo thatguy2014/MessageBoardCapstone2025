11cc451b44b6b38e8514ec389956f18da3446a7b
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-13 22:19:06 -0500

    made it so it returns result
    turns out mysqli real query returns a bool but mysqli query returns the result so I changed it to that

--- sql.php
+++ sql.php
@@ -11,15 +11,14 @@ try {
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
         mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
         
-        $getResults = mysqli_real_query($conn, $tsql);
-        $getResults = mysqli_use_result($conn)
-
-        while ($row=mysqli_fetch_row($result))
+        $result = mysqli_query($conn, $tsql);
+        print($result);
+        /*while ($row=mysqli_fetch_row($result))
         {
         printf ("%s (%s)\n",$row[0],$row[1]);
         }
         // Free result set
-        mysqli_free_result($result);
+        mysqli_free_result($result);*/
         
 
 

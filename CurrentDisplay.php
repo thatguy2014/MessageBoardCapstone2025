8c52893a494f139c18d09c4a6dedbf838c9503ce
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-18 18:39:18 -0500

    getting rid of redundancy
    it wasn't working so I'm getting rid of some extra things to verify it is still working

--- CurrentDisplay.php
+++ CurrentDisplay.php
@@ -1,19 +1,14 @@
 <?php
-    $url1=$_SERVER['REQUEST_URI'];
+    /*$url1=$_SERVER['REQUEST_URI'];
     header("Refresh: 60; URL=$url1");*/         //this should autorefresh
-   
-    //include('sql.php');               //I will need to go back to this but want to test
 
     $conn = mysqli_init();
-
-    $tsql = "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = 1";
-
     mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
     mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
     mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
 
     //Run the Select query
-    $res = mysqli_query($conn, 'SELECT CurrentDisplay FROM CurrentDisplays');
+    $res = mysqli_query($conn, 'SELECT CurrentDisplay FROM CurrentDisplays');   
 ?>
 
 <html>

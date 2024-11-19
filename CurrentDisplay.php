22f61ecf436b33d89d64cbc81bd01d1b447bbb52
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-18 20:11:13 -0500

    performed cleanup
    changed login for insert.php and added auto refresh back in and got rid of comments and such that weren't used

--- CurrentDisplay.php
+++ CurrentDisplay.php
@@ -1,23 +1,13 @@
 <?php
-    /*$url1=$_SERVER['REQUEST_URI'];
-    header("Refresh: 60; URL=$url1");*/         //this should autorefresh
-   
-    //include('sql.php');               //I will need to go back to this but want to test
+    $url1=$_SERVER['REQUEST_URI'];
+    header("Refresh: 60; URL=$url1");         //this should autorefresh
 
     $conn = mysqli_init();
-
-    $tsql = "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = 1";
-
     mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
     mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
     mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "qzmbodniyz", "YgM0Smd\$bLYYepT1", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
 
-    //Run the Select query
-    //printf("Reading data from table: \n");
     $res = mysqli_query($conn, 'SELECT CurrentDisplay FROM CurrentDisplays');
-    //while ($row = mysqli_fetch_assoc($res)) {
-    //    printf ("%s \n", $row["CurrentDisplay"]);
-    //} 
 ?>
 
 <html>

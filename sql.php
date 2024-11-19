7225b238b5cfbed7a335f34f4edb6ccd9637cb57
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-18 20:04:30 -0500

    trying different user
    I'm trying the login for the other user to see if that works

--- sql.php
+++ sql.php
@@ -5,7 +5,7 @@
 
         mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
         mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-        mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
+        mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "qzmbodniyz", "YgM0Smd\$bLYYepT1", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
 
         //Run the Select query
     printf("Reading data from table: \n");

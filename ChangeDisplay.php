8f9b2b051eecc68c54156bf2bddec4b09c1b8a7b
Aiden Tallet <112090371+thatguy2014@users.noreply.github.com>
2024-11-18 19:03:24 -0500

    Revert "removing php from ChangeDisplay.php"
    This reverts commit dfdc41afe09d1afd1ebccdbc71e21f9bfcc6ba44.

--- ChangeDisplay.php
+++ ChangeDisplay.php
@@ -1,5 +1,11 @@
 <?php
 
+    //connection data
+    $conn = mysqli_init();
+    mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+    mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "PHPLogin", "OctoberNovemberUniform", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
+
 ?>
 
 <html>

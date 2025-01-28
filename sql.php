6d7672406d34effd234e78878546fe88a4d12765
Aiden Tallet <a-tallet@onu.edu>
2025-01-28 14:11:12 -0500

    Created basic Register.php
    started using this tutorial to make the login process: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
    I created a sql.php to handle the actual database login and then the php in Register.php should register a new user

--- sql.php
+++ sql.php
@@ -1,17 +1,21 @@
 <?php
-        $conn = mysqli_init();
 
-        $tsql = "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = 1";
+        define('DB_SERVER', 'mbcwebbapp-server.mysql.database.azure.com');
+        define('DB_USERNAME', 'qzmbodniyz');
+        define('DB_PASSWORD', "YgM0Smd\$bLYYepT1");
+        define('DB_NAME', 'mbcwebbapp-database');
 
-        mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
-        mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-        mysqli_real_connect($conn, "mbcwebbapp-server.mysql.database.azure.com", "qzmbodniyz", "YgM0Smd\$bLYYepT1", "mbcwebbapp-database", 3306, MYSQLI_CLIENT_SSL);
+        $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+        if($link === false){
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+/*
         //Run the Select query
     printf("Reading data from table: \n");
     $res = mysqli_real_query($conn, 'SELECT CurrentDisplay FROM CurrentDisplays');
     $res = mysqli_query($conn, 'SELECT CurrentDisplay FROM CurrentDisplays');
     while ($row = mysqli_fetch_assoc($res)) {
         printf ("%s \n", $row["CurrentDisplay"]);
-    }
+    }*/
 ?>

d281ea592a97e7e00a884a3af63330909aaf01c1
Aiden Tallet <a-tallet@onu.edu>
2025-01-28 18:23:13 -0500

    fixing sql.php
    this should verify that $link stays as the MySQLi object

--- sql.php
+++ sql.php
@@ -1,18 +1,21 @@
 <?php
 
-        define('DB_SERVER', 'mbcwebbapp-server.mysql.database.azure.com');
-        define('DB_USERNAME', 'qzmbodniyz');
-        define('DB_PASSWORD', "YgM0Smd\$bLYYepT1");
-        define('DB_NAME', 'mbcwebbapp-database');
+define('DB_SERVER', 'mbcwebbapp-server.mysql.database.azure.com');
+define('DB_USERNAME', 'qzmbodniyz');
+define('DB_PASSWORD', "YgM0Smd\$bLYYepT1");
+define('DB_NAME', 'mbcwebbapp-database');
 
 
-        $conn = mysqli_init();
-        mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
-        mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-        global $link;
-        $link = mysqli_real_connect($conn, DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, 3306, MYSQLI_CLIENT_SSL);
-        
-        if (mysqli_connect_errno()) {
-            die("ERROR: Could not connect. " . mysqli_connect_error());
-        }
+$conn = mysqli_init();
+mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+// Connect to MySQL
+$link = mysqli_real_connect($conn, DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, 3306, MYSQLI_CLIENT_SSL);
+
+if (!$link) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Now $link contains the MySQLi object
 ?>

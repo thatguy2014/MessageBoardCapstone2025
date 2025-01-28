49f2eb309386afe70c552ea9ecf7e4c60984c28a
Aiden Tallet <a-tallet@onu.edu>
2025-01-28 16:00:38 -0500

    updated Register.php
    fixed the adduser sql statement. Fixed the password so it doesn't hash twice.
    made sure the php exits when redirecting to login page

--- Register.php
+++ Register.php
@@ -67,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "DECLARE @responseMessage nvarchar(max); execute AddUser \"?\", \"?\",@responseMessage;"
+        $sql = "call AddUser (\"?\", \"?\");";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -75,12 +75,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             
             // Set parameters
             $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_password = $password;
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
                 header("location: Login.php");
+                exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }

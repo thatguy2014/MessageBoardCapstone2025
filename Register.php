910abbcf11121c73187d035b5867ef490ff590fc
Aiden Tallet <a-tallet@onu.edu>
2025-01-28 14:15:48 -0500

    testing php not working
    verifying that the html works and the php is the issue

--- Register.php
+++ Register.php
@@ -1,4 +1,4 @@
-<?php
+<?php/*
 // Include config file
 require_once "sql.php";
  
@@ -27,7 +27,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                /* store result */
+                // store result 
                 mysqli_stmt_store_result($stmt);
                 
                 if(mysqli_stmt_num_rows($stmt) == 1){
@@ -92,7 +92,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Close connection
     mysqli_close($link);
-}
+}*/
 ?>
  
 <!DOCTYPE html>

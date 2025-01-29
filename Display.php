ad120d2c37401d3ebcab4d2658abbbd90b539c27
Aiden Tallet <a-tallet@onu.edu>
2025-01-29 14:55:01 -0500

    fixing display.php errors
    I think pulling the session id was causing errors so I reworked it and it should work now, also added more debugging in case it doesn't work

--- Display.php
+++ Display.php
@@ -1,9 +1,13 @@
 <?php
+
+    // starts displaying errors when things go wrong
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
     session_start();            //this is a test
     require_once "sql.php";
     //the following lines should connect to the database and run the query
     
-    $res = mysqli_query($conn, "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = $_SESSION["UserId"]");
+    $res = mysqli_query($conn, "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = '" . $_SESSION['UserId'] . "'");
 ?>
 <!--the style is so the fullscreen view looks right-->
 <style>

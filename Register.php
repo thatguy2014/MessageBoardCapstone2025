5790b289b33fe71e899ca34492dcf6cdbf7e1ed1
Aiden Tallet <a-tallet@onu.edu>
2025-02-03 14:46:44 -0500

    got rid of reset button in register
    wasn't working and also felt redundant

--- Register.php
+++ Register.php
@@ -158,10 +158,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-            </div>
             <p>Already have an account? <a href="login.php">Login here</a>.</p>
         </form>
     </div>    

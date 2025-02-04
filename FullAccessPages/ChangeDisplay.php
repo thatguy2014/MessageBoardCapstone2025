f0d9f84d6024b77e9c1d4e273d0ea1415d4859f1
Aiden Tallet <a-tallet@onu.edu>
2025-02-04 14:37:05 -0500

    changing display of ChangeDisplay.php
    this should have it line up with the other pages

--- FullAccessPages/ChangeDisplay.php
+++ FullAccessPages/ChangeDisplay.php
@@ -5,20 +5,23 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
     <head>
         <link rel="stylesheet" type="text/css" href="/../css/Main.css" />
     </head>
-    <header>
-        <h1>Office Message Board Custom Display</h1>
-    </header>
-    <?php include '/home/site/wwwroot/templates/navbar.html'; ?>
+    
+    
     <body>
-    <form action="insert.php" method="post"> 
-            <p>
-                <label for="Input">Input what you'd like to display:</label>
-                <input type="text" name="selected_input" id="Input">
-            </p>
-            
-            <input type="submit" value="Submit">
-    </form>
-
+        <div>
+            <header>
+                <h1>Office Message Board Custom Display</h1>
+            </header>
+            <?php include '/home/site/wwwroot/templates/navbar.html'; ?>
+            <form action="insert.php" method="post"> 
+                    <p>
+                        <label for="Input">Input what you'd like to display:</label>
+                        <input type="text" name="selected_input" id="Input">
+                    </p>
+                    
+                    <input type="submit" value="Submit">
+            </form>
+        </div>
     </body>
     <footer>
         

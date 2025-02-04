3af7d488a8f44391d668879deec0eab4563b5e70
Aiden Tallet <a-tallet@onu.edu>
2025-02-04 14:42:45 -0500

    fixing a small login error
    previously if you logged in and then went to display only and logged in there without signing out, when you went to current display again you'd be logged into the display only account. This should fix that

--- ScriptFiles/VerifyLogin.php
+++ ScriptFiles/VerifyLogin.php
@@ -4,6 +4,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    $_SESSION["Displayloggedin"] = false;
     header("location: /../LoginPages/login.php");
     exit;
 }
--- ScriptFiles/VerifyViewOnlyLogin.php
+++ ScriptFiles/VerifyViewOnlyLogin.php
@@ -4,6 +4,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["Displayloggedin"]) || $_SESSION["Displayloggedin"] !== true){
+    $_SESSION["loggedin"] = false;
     header("location: /../DisplayOnlyPages/DisplayOnlyLogin.php");
     exit;
 }

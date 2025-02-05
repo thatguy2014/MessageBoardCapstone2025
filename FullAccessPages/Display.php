75ebbd0035309fb02f12f87aded3b3c28cf999d6
Aiden Tallet <a-tallet@onu.edu>
2025-02-05 15:52:49 -0500

    adjusting display.php (small typos)

--- FullAccessPages/Display.php
+++ FullAccessPages/Display.php
@@ -7,14 +7,14 @@
     require_once "/home/site/wwwroot/ScriptFiles/sql.php";
     //the following lines should connect to the database and run the query
     $userid = $_SESSION["UserId"];
-    $stmt = mysqli_prepare($conn, "SELECT ImageView FROM userinfo WHERE UserId = '?'");
+    $stmt = mysqli_prepare($conn, "SELECT ImageView FROM userinfo WHERE UserId = ?");
     $res = mysqli_query($conn, "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = '" . $userid . "'");
     
     mysqli_stmt_bind_param($stmt, "i", $userid);
 
     if(mysqli_stmt_execute($stmt)) {
-        mysqli_stmt_store_result($ImageView);
-        mysqli_stmt_bind_result($ImageView, $resultbool);
+        mysqli_stmt_store_result($stmt);
+        mysqli_stmt_bind_result($stmt, $resultbool);
         mysqli_stmt_fetch($stmt);
     }
 

e8634b6e5eaa4404ba2f192f888ca7d230cf97c1
Aiden Tallet <a-tallet@onu.edu>
2025-02-05 13:37:32 -0500

    added better debugging
    the file is uploading but the filepath isn't being saved in the sql server. This should print what the actual sql command is going through as and should also add some spacing the current debugging so its easier to read

--- ScriptFiles/upload.php
+++ ScriptFiles/upload.php
@@ -28,36 +28,36 @@
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
+            echo "File is an image - " . $check["mime"] . ". \n";
             $uploadOk = 1;
         } else {
-            echo "File is not an image.";
+            echo "File is not an image. \n";
             $uploadOk = 0;
         }
     }
 
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        echo "Sorry, file already exists. \n";
         $uploadOk = 0;
     }
 
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+        echo "Sorry, your file is too large. \n";
         $uploadOk = 0;
     }
 
         // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed. \n";
         $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "Sorry, your file was not uploaded. \n";
         // if everything is ok, try to upload file
     } else {
 
@@ -66,14 +66,16 @@
             //need to store the file location
             $userid = $_SESSION["UserId"];
             try{
+                echo "The sql query is: UPDATE CurrentDisplays SET ImageLocation = $target_file WHERE UserId = $userid \n";
                 mysqli_query($conn, "UPDATE CurrentDisplays SET ImageLocation = $target_file WHERE UserId = $userid;");
             } catch(Exception $e) {
                 error_log("Error occured while updating database with file location " . $e->getMessage());
             }
-            echo "<p>Message updated successfully please click here <a href=/../FullAccessPages/currentdisplay.php>Go Back</a>.</p>";
+            echo "<p>Message updated successfully please click here <a href=/../FullAccessPages/currentdisplay.php>Go Back</a>. \n</p>";
             header("location: /../FullAccessPages/currentdisplay.php");
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "Sorry, there was an error uploading your file. 
+            n";
         }
     }
 ?>

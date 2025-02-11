5f1f37d8c8c6c09f7381e6e0668c466804d47821
Aiden Tallet <a-tallet@onu.edu>
2025-02-10 20:00:57 -0500

    fixing register page

--- LoginPages/Register.php
+++ LoginPages/Register.php
@@ -159,6 +159,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
             <p>Already have an account? <a href="login.php">Login here</a>.</p>
         </form>
     </div>    

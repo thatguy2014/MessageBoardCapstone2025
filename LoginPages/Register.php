a771ba8b3a8ac55d217fb88ba0d5afd169b566f4
Aiden Tallet <a-tallet@onu.edu>
2025-02-10 20:03:36 -0500

    fixing register button

--- LoginPages/Register.php
+++ LoginPages/Register.php
@@ -160,7 +160,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
+                <input type="submit" class="btn btn-primary" value="Register">
             </div>
             <p>Already have an account? <a href="login.php">Login here</a>.</p>
         </form>

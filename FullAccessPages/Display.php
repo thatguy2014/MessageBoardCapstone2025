da5f9b21433268b020835a3914cba8592bfb2831
Aiden Tallet <a-tallet@onu.edu>
2025-02-10 19:52:14 -0500

    allowed you to change displayed font
    still no way to change it in settings but it will display

--- FullAccessPages/Display.php
+++ FullAccessPages/Display.php
@@ -15,17 +15,16 @@
     require_once "/home/site/wwwroot/ScriptFiles/sql.php";
     //the following lines should connect to the database and run the query
     $userid = $_SESSION["UserId"];
-    $stmt = mysqli_prepare($conn, "SELECT ImageView FROM userinfo WHERE UserId = ?");
-    $res = mysqli_query($conn, "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = '" . $userid . "'");
     
+    $res = mysqli_query($conn, "SELECT CurrentDisplay FROM CurrentDisplays WHERE UserId = '" . $userid . "'");
+    //all image things
+    $stmt = mysqli_prepare($conn, "SELECT ImageView FROM userinfo WHERE UserId = ?");
     mysqli_stmt_bind_param($stmt, "i", $userid);
-
     if(mysqli_stmt_execute($stmt)) {
         mysqli_stmt_store_result($stmt);
         mysqli_stmt_bind_result($stmt, $resultbool);
         mysqli_stmt_fetch($stmt);
     }
-
     $ImageStmt = mysqli_prepare($conn, "SELECT ImageLocation FROM currentdisplays where UserId = ?");
     mysqli_stmt_bind_param($ImageStmt, "i", $userid);
     if (mysqli_stmt_execute($ImageStmt)) {
@@ -33,9 +32,12 @@
         mysqli_stmt_bind_result($ImageStmt, $ImageDir);
         mysqli_stmt_fetch($ImageStmt);
     }
-
     $ImageDir = substr($ImageDir, 18);
     $ImageDir = "/.." . $ImageDir;
+
+    //font family sql
+    $Font = mysqli_query($conn, "SELECT Font FROM userinfo WHERE UserId = '" . $userid . "'");
+    $UserFont = mysqli_fetch_assoc($Font);
 ?>
 <!--the style is so the fullscreen view looks right-->
 <style>
@@ -44,8 +46,8 @@ html {
 }
 h2 {
   font-size: 10em;
+  font-family: <?php echo "$UserFont" ?>;
 }
-
     </style>
 <html>
     <h2 id="currentdisplay" style="background:white">

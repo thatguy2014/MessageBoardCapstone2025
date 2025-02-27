e659d7422ec67d344111127163cf66aee23504ec
Aiden Tallet <a-tallet@onu.edu>
2025-02-27 15:29:32 -0500

    getting rid of error reporting

--- FullAccessPages/ChangeDisplay.php
+++ FullAccessPages/ChangeDisplay.php
@@ -2,8 +2,8 @@
 require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
 
 // Enable error reporting for debugging (comment out in production)
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 ?>
 <html>
     <head>

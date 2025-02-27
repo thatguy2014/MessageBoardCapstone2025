471441c0f34eb44e3ce7460aca2737c34cdb6c5a
Aiden Tallet <a-tallet@onu.edu>
2025-02-27 12:35:58 -0500

    condensing update logic
    made it all one page and got rid of the other buttons, I'm not sure if the update logic will work or not though

--- FullAccessPages/ChangeDisplay.php
+++ FullAccessPages/ChangeDisplay.php
@@ -13,9 +13,10 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
                 <h1>Office Message Board Change Display</h1>
             </header>
             <?php include '/home/site/wwwroot/templates/navbar.html'; ?>
-            <form action="insert.php" method="post"> 
+            <form action="insert.php" method="post" id="changedisplay"> 
                     <p> 
                         <label for="TypeSpinner">Select what type of display you'd like to input:</label>
+                        <input type="hidden" name="no_input" value="">
                         <select id="TypeSpinner" name="InputType" onchange="displayQuestion(this.value)">
                             <option value="CustomText">Custom Text</option>
                             <option value="PresetText">Preset Text</option>
@@ -24,11 +25,26 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
                     </p>
 
                     <div id = "PresetText" style = "display:none;">
-                        <p>Preset Text</p>
+                        <label for = "presets">Select what preset you'd like to display</label>
+                        <input type="hidden" name="selected_input" value="">
+                        <select name="presets" id="presets" onchange="setSelectedValue(this)">
+                            <option value="">Select...</option>
+                            <option value="I'll be back in 5">I'll be back in 5</option>
+                            <option value="I'm off campus for the rest of the day">I'm off campus for the rest of the day</option>
+                            <option value="I'll be back soon">I'll be back soon</option>
+                            <option value="I'm in class and will be back after">I'm in class and will be back after</option>
+                        </select>
+
+                        <p>or select a custom preset:</p>
+                        <input type = "hidden" name="selected_input" value="">
+                        <select name="custompresets" id="custompresets" onchange="setSelectedValue(this)">
+                            <!-- need php to handle the database custom preset pulling-->
+                        </select>
                     </div>
 
                     <div id = "Image" style = "display:none;">
-                        <p>Image Text</p>
+                        <label for="fileToUpload">Select image to upload (max 500kb):</label>
+                        <input type="file" name="fileToUpload" id="fileToUpload">
                     </div>
 
 
@@ -39,7 +55,7 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
                         </p>
                     </div>
                     
-                    <input type="submit" value="Submit">
+                    <input type="submit" value="Submit" disabled>
             </form>
         </div>
     </body>
@@ -59,6 +75,39 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
             }
             if (answer != "CustomText") {
                 document.getElementById('CustomText').style.display = "none";
+            }
+        }
+
+        function setSelectedValue(select) {
+            var selectedValue = select.value;
+            
+            // Update the input value
+            document.getElementById('changedisplay').querySelector('[name="selected_input"]').value = selectedValue;
+
+            // Reset other preset dropdowns
+            document.querySelectorAll('#changedisplay select[name="presets"], #changedisplay select[name="custompresets"]').forEach(function(presetSelect) {
+                if (presetSelect !== select) {
+                    presetSelect.value = '';
+                }
+            });
+
+            // Update form validation
+            validateForm();
+        }
+
+        function validateForm() {
+            var form = document.getElementById('changedisplay');
+            var submitButton = form.querySelector('input[type="submit"]');
+            
+            if (submitButton.disabled) {
+                submitButton.disabled = false;
+            } else {
+                var selectedValue = form.querySelector('[name="selected_input"]').value; //this may need fixed, idk if it'll work right
+                if (selectedValue === "") {
+                    submitButton.disabled = true;
+                } else {
+                    submitButton.disabled = false;
+                }
             }
         }
     </script>

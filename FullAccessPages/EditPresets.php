04fed84de8844e4ff47b4f27bfd1567798e5e501
Aiden Tallet <a-tallet@onu.edu>
2025-02-27 16:03:59 -0500

    added new Edit Custom Presets Page

---
+++ FullAccessPages/EditPresets.php
@@ -0,0 +1,25 @@
+<?php
+require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
+
+// Enable error reporting for debugging (comment out in production)
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/../css/Main.css" />
+    </head>
+    
+    
+    <body>
+        <div>
+            <header>
+                <h1>Office Message Board Edit Custom Presets</h1>
+            </header>
+            <?php include '/home/site/wwwroot/templates/navbar.html'; ?>
+        </div>
+        <p>
+            this is going to have some way to edit your custom presets
+        </p>
+
+    </body>

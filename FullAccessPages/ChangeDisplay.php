0a74c78f2758e4938bb06addec9ba93e64a336b1
Aiden Tallet <a-tallet@onu.edu>
2025-02-27 12:04:32 -0500

    consolidating update display pages
    started to combine the update display pages to be one page for simplicity

--- FullAccessPages/ChangeDisplay.php
+++ FullAccessPages/ChangeDisplay.php
@@ -10,14 +10,34 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
     <body>
         <div>
             <header>
-                <h1>Office Message Board Custom Text Display</h1>
+                <h1>Office Message Board Change Display</h1>
             </header>
             <?php include '/home/site/wwwroot/templates/navbar.html'; ?>
             <form action="insert.php" method="post"> 
-                    <p>
-                        <label for="Input">Input what you'd like to display (250 chararcter limit):</label>
-                        <input type="text" name="selected_input" id="Input" maxlength="250">
+                    <p> 
+                        <label for="TypeSpinner">Select what type of display you'd like to input:</label>
+                        <select id="TypeSpinner" name="InputType" onchange="displayQuestion(this.value)">
+                            <option value="CustomText">Custom Text</option>
+                            <option value="PresetText">Preset Text</option>
+                            <option value="Image">Image</option>
+                        </select>
                     </p>
+
+                    <div id = "PresetText" style = "display:none;">
+                        <p>Preset Text</p>
+                    </div>
+
+                    <div id = "Image" style = "display:none;">
+                        <p>Image Text</p>
+                    </div>
+
+
+                    <div id="CustomText" style ="display:none;">
+                        <p>
+                            <label for="Input">Input what you'd like to display (250 chararcter limit):</label>
+                            <input type="text" name="selected_input" id="Input" maxlength="250">
+                        </p>
+                    </div>
                     
                     <input type="submit" value="Submit">
             </form>
@@ -26,3 +46,19 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
     <footer>
         
     </footer>
+
+
+    <script>
+        function displayQuestion(answer) {
+            document.getElementById(answer).style.display = "block";
+            if (answer != "PresetText") { // hide the div that is not selected
+                document.getElementById('PresetText').style.display = "none";
+            }
+            if (answer != "Image") {
+                document.getElementById('Image').style.display = "none";
+            }
+            if (answer != "CustomText") {
+                document.getElementById('CustomText').style.display = "none";
+            }
+        }
+    </script>

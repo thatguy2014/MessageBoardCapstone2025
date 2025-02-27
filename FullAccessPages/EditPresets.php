47ee1553fb08f9511a06ba2903762fc673ffc3ce
Aiden Tallet <a-tallet@onu.edu>
2025-02-27 16:34:46 -0500

    allowed for display of presets in dropdown
    made it so a query for custom presets should be run and then display all of your presets as options for deleting a preset. Also just made the general form

--- FullAccessPages/EditPresets.php
+++ FullAccessPages/EditPresets.php
@@ -1,9 +1,12 @@
 <?php
 require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
-
+require_once "/home/site/wwwroot/ScriptFiles/sql.php";
 // Enable error reporting for debugging (comment out in production)
 //ini_set('display_errors', 1);
 //error_reporting(E_ALL);
+
+$userid = $_SESSION["UserId"];
+
 ?>
 <html>
     <head>
@@ -17,9 +20,48 @@ require_once "/home/site/wwwroot/ScriptFiles/VerifyLogin.php";
                 <h1>Office Message Board Edit Custom Presets</h1>
             </header>
             <?php include '/home/site/wwwroot/templates/navbar.html'; ?>
+
+            <p>
+            this is going to display all of your current presets
+            </p>
+            <form action="/../ScriptFiles/EditPresetScript.php" method="post" id="editpresets"> 
+                <div id="addPresetDiv" style ="display:block;">
+                    <p>
+                        <label for="addPresetinput">Input what text you would like to be a preset(max characters: 250)</label>
+                        <input type="text" name="addPresetinput" id="addPresetinput" maxlength="250" onchange="validateForm()">
+                    </p>
+                </div>
+
+                <div id = "deletePresetDiv" style = "display:block;">
+                    <label for = "deletePresetInput">Select what preset you'd like to delete</label>
+                    <input type = "hidden" name="deletePresetInput_hidden" value="">
+                    <select name="deletePresetInput" id="deletePresetInput" onchange="setSelectedValue(this)">
+                        <option value="">Select...</option>
+                        <option value="custompresettest">CustomPresetTest</option>
+                        <?php
+                            //need sql to pull all of the current custompreset options
+                            $presetsQuery = mysqli_prepare($conn, "SELECT PresetString FROM presets WHERE UserId = ?");
+                            mysqli_stmt_bind_param($presetsQuery, "i", $userid);
+                            if(mysqli_stmt_execute($presetsQuery)) {
+                                mysqli_stmt_store_result($presetsQuery);
+                                mysqli_stmt_bind_result($presetsQuery, $presetsResult);
+                                mysqli_stmt_fetch($presetsQuery);
+                            }
+                            while ($row = mysqli_fetch_assoc($res)) {
+                                $content = "<option value=\"";
+                                $content .= $row["PresetString"] . "\"> " . $row["PresetString"] . "</option>";
+                                echo $content;
+                            }
+                            $content = "";
+                        ?>
+                    </select>
+                </div>
+                <input type="submit" value="Submit" disabled>
+            </form>
+
+
+
         </div>
-        <p>
-            this is going to have some way to edit your custom presets
-        </p>
+        
 
     </body>

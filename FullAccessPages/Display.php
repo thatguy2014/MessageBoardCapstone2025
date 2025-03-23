ab8de2d606b41576ed96792643fe3d9bfb0a18ee
Aiden Tallet <a-tallet@onu.edu>
2025-03-23 19:15:40 -0400

    more fullscreen fixes!

--- FullAccessPages/Display.php
+++ FullAccessPages/Display.php
@@ -297,6 +297,7 @@ h2, p {
         }
 
         function exitFullscreen() {
+            window.parent.postMessage("exitFullscreen", "*");
             elem.contentWindow.postMessage("exitFullscreen", "*");
         }
 

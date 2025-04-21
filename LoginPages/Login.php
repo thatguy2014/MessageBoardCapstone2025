a83d99f813024056ed27107781c740ddbd221c69
Aiden Tallet <a-tallet@onu.edu>
2025-04-20 23:14:35 -0400

    getting rid of display only link

--- LoginPages/Login.php
+++ LoginPages/Login.php
@@ -137,7 +137,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+            <!-- Deprecated link
             <p>Is this a display only device? <a href="/../DisplayOnlyPages/DisplayOnlyLogin.php">Login Here</a>.</p>
+    -->
         </form>
     </div>
 </body>
